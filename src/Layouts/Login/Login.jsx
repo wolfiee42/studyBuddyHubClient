fba814e19fc0dcd65543481130386d250b330a70
wolfiee42 <saifalislam2022@gmail.com>
2023-11-05 23:28:05 +0600

    final touch on login and register layout

--- src/Layouts/Login/Login.jsx
+++ src/Layouts/Login/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
 
     const handleloginform = event => {
@@ -14,7 +16,7 @@ const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div>
-                <h1 className="text-5xl font-bold text-center mb-10">Login now!</h1>
+                <h1 className="text-5xl font-bold font-serif text-center mb-10"><span className="text-yellow-400">Login</span> now!</h1>
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
                         <img src="https://i.ibb.co/ryMfKhb/undraw-Login-re-4vu2-1-removebg-preview.png" alt="" />
@@ -23,13 +25,13 @@ const Login = () => {
                         <form onSubmit={handleloginform} className="card-body">
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text text-base">Email</span>
                                 </label>
                                 <input name="email" type="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text text-base">Password</span>
                                 </label>
                                 <input name="password" type="password" placeholder="password" className="input input-bordered" required />
                                 <label className="label">
@@ -39,6 +41,7 @@ const Login = () => {
                             <div className="form-control mt-6">
                                 <button className="btn w-full bg-yellow-400 hover:bg-yellow-500 hover:text-white">Login</button>
                             </div>
+                            <p className="text-xs">New into this website? Please <Link className="text-yellow-500 hover:underline" to={'/register'}>Register</Link></p>
                         </form>
                     </div>
                 </div>
--- src/Layouts/Register/Register.jsx
+++ src/Layouts/Register/Register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
 
@@ -15,7 +16,7 @@ const Register = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div>
-                <h1 className="text-5xl font-bold text-center mb-10">Register now!</h1>
+                <h1 className="text-5xl font-bold text-center mb-10 font-serif"><span className="text-yellow-400">Register</span> now!</h1>
                 <div className="hero-content flex-col lg:flex-row">
                     <div className="text-center lg:text-left">
                         <img src="https://i.ibb.co/rvP3gfK/undraw-Sign-up-n6im-removebg-preview-removebg-preview.png" alt="" />
@@ -24,31 +25,32 @@ const Register = () => {
                         <form onSubmit={handleRegisterForm} className="card-body">
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Name</span>
+                                    <span className="label-text text-base">Name</span>
                                 </label>
                                 <input type="text" name="name" placeholder="Your Name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Image</span>
+                                    <span className="label-text text-base">Image</span>
                                 </label>
                                 <input type="text" name="image" placeholder="imageURL" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text text-base">Email</span>
                                 </label>
                                 <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text text-base">Password</span>
                                 </label>
                                 <input name="password" type="password" placeholder="password" className="input input-bordered" required />
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn w-full bg-yellow-400 hover:bg-yellow-500 hover:text-white">Register</button>
                             </div>
+                            <p className="text-xs">Already have an account? Please <Link className="text-yellow-500 hover:underline" to={'/login'}>Login</Link></p>
                         </form>
                     </div>
                 </div>
