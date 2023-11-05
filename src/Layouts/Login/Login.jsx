a669e6ca702c1b184c2f1a916c3432a84ebd79f8
wolfiee42 <saifalislam2022@gmail.com>
2023-11-05 23:17:08 +0600

    login & registration page with function

--- src/Layouts/Login/Login.jsx
+++ src/Layouts/Login/Login.jsx
@@ -1,4 +1,16 @@
 const Login = () => {
+
+    const handleloginform = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        const user = { email, password };
+        console.log(user);
+    }
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div>
@@ -8,18 +20,18 @@ const Login = () => {
                         <img src="https://i.ibb.co/ryMfKhb/undraw-Login-re-4vu2-1-removebg-preview.png" alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form onSubmit={handleloginform} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input name="email" type="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input name="password" type="password" placeholder="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
--- src/Layouts/Register/Register.jsx
+++ src/Layouts/Register/Register.jsx
@@ -1,5 +1,17 @@
 
 const Register = () => {
+
+    const handleRegisterForm = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const image = form.image.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        const user = { name, image, email, password };
+        console.log(user);
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div>
@@ -9,30 +21,30 @@ const Register = () => {
                         <img src="https://i.ibb.co/rvP3gfK/undraw-Sign-up-n6im-removebg-preview-removebg-preview.png" alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form onSubmit={handleRegisterForm} className="card-body">
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text">Name</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="text" name="name" placeholder="Your Name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Image</span>
+                                </label>
+                                <input type="text" name="image" placeholder="imageURL" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input name="password" type="password" placeholder="password" className="input input-bordered" required />
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn w-full bg-yellow-400 hover:bg-yellow-500 hover:text-white">Register</button>
