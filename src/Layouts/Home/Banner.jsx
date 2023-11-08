17f15d693868e5680bb848bf8ec05266f0ee025a
wolfiee42 <saifalislam2022@gmail.com>
2023-11-08 21:47:30 +0600

    made responsive banner section

--- src/Layouts/Home/Banner.jsx
+++ src/Layouts/Home/Banner.jsx
@@ -4,7 +4,7 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 const Banner = () => {
     return (
-        <Swiper className='max-w-7xl mx-auto text-white'
+        <Swiper className='max-w-7xl mx-auto mt-32 lg:mt-0 text-white'
         spaceBetween={50}
         slidesPerView={1}
         // onSlideChange={() => console.log('slide change')}
@@ -22,20 +22,20 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         >
             <SwiperSlide className='relative mx-auto'>
-                <img className='w-[1100px] h-[600px] mx-auto rounded-xl' src="https://i.ibb.co/MRJzTZt/essay-Writing.jpg" alt="" />
-                <div className='absolute opacity-70 h-full  flex items-center w-[500px] left-[90px] rounded-l-lg top-0 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]'>
+                <img className='w-[500px] h-[230px] md:w-[768px] md:h-[430px] lg:w-[1100px] lg:h-[600px] mx-auto rounded-xl' src="https://i.ibb.co/MRJzTZt/essay-Writing.jpg" alt="" />
+                <div className='absolute opacity-70 h-full  flex items-center w-[500px] lg:left-[90px] top-0 rounded-l-lg gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]'>
                     <div className='pl-10'>
-                        <h2 className='text-5xl font-bold'>Study Buddy Hub</h2>
-                        <p className='text-xl mt-5'>Together We Learn, Together We Grow</p>
+                        <h2 className='text-2xl md:text-5xl font-bold'>Study Buddy Hub</h2>
+                        <p className='md:text-xl mt-5'>Together We Learn, Together We Grow</p>
                     </div>
                 </div>
             </SwiperSlide>
             <SwiperSlide className='relative mx-auto'>
-                <img className='w-[1100px] h-[600px] mx-auto rounded-xl' src="https://i.ibb.co/W5mFPKc/mathematics.jpg" alt="" />
-                <div className='absolute opacity-70 h-full  flex items-center w-[500px] left-[90px] rounded-l-lg top-0 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]'>
+                <img className='w-[500px] h-[230px] md:w-[768px] md:h-[430px] lg:w-[1100px] lg:h-[600px] mx-auto rounded-xl' src="https://i.ibb.co/W5mFPKc/mathematics.jpg" alt="" />
+                <div className='absolute opacity-70 h-full  flex items-center w-[500px] lg:left-[90px] rounded-l-lg top-0 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]'>
                     <div className='pl-10'>
-                        <h2 className='text-5xl font-bold'>Study Buddy Hub</h2>
-                        <p className='text-xl mt-5'>Together We Learn, Together We Grow</p>
+                        <h2 className='text-2xl md:text-5xl font-bold'>Study Buddy Hub</h2>
+                        <p className='md:text-xl mt-5'>Together We Learn, Together We Grow</p>
                     </div>
                 </div>
             </SwiperSlide>
